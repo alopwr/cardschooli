0723ff5158a574be54a3f40cd6cbed25ad584036
m4k5 <maks@riseup.net>
2018-01-01 14:16:09 +0100

    showing 2nd 'choose file' windo

--- bin/cardschooli.py
+++ bin/cardschooli.py
@@ -1,6 +1,6 @@
 import sys
 
-from PyQt5.QtWidgets import QApplication, QWidget, QDesktopWidget, QPushButton
+from PyQt5.QtWidgets import QApplication, QWidget, QDesktopWidget, QPushButton, QLineEdit, QLabel, QFileDialog
 
 
 class Window0(QWidget):
@@ -13,13 +13,44 @@ class Window0(QWidget):
         self.setWindowTitle('cardschooli - witaj!')
         self.resize(800, 600)
         center(self)
+        self.project_name = QLineEdit(self)
+        self.project_name.setToolTip('Wprowadź dowolną nazwę projektu.')
+        self.project_name.move(275, 200)
+        self.project_name.resize(250, 25)
         start_btn = QPushButton('Rozpocznij tworzenie talii >>>', self)
         start_btn.setGeometry(275, 275, 250, 50)
         self.show()
         start_btn.clicked.connect(self.next)
 
     def next(self):
+        self.project = self.project_name.text()
+        print(self.project)
         self.close()
+        window1.init_ui()
+        window1.show()
+
+
+class Window1(QWidget):
+
+    def __init__(self):
+        super().__init__()
+
+    def init_ui(self):
+        self.setWindowTitle('cardschooli - krok 1')
+        self.resize(800, 600)
+        center(self)
+        QLabel(
+            'Wybierz plik *.csv z danymi do projektu {}. Dane zostaną skopiowane, plik nie zostanie naruszony.'.format(
+                window0.project), self)
+        open_btn = QPushButton('Otwórz plik', self)
+        open_btn.clicked.connect(self.open_file)
+
+    def open_file(self):
+        options = QFileDialog.Options()
+        options |= QFileDialog.DontUseNativeDialog
+        filename, _ = QFileDialog.getOpenFileName(self, "Wybierz swój plik z danymi", "",
+                                                  "dane do cardschooli (*.csv)", options=options)
+        self.filename = filename
 
 
 def center(window):
@@ -31,4 +62,5 @@ def center(window):
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     window0 = Window0()
+    window1 = Window1()
     sys.exit(app.exec_())

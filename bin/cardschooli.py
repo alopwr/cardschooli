d9ff34eaca2105703d250d1cffc0f3659978e1f2
m4k5 <maks@riseup.net>
2018-01-23 21:50:46 +0100

    imports csv, reads data, generates and shows preview, saves preview.png in card folder

--- bin/cardschooli.py
+++ bin/cardschooli.py
@@ -1,9 +1,17 @@
+import csv
+import os.path
 import sys
 
+from PIL import Image, ImageDraw
+from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import QApplication, QWidget, QDesktopWidget, QPushButton, QLineEdit, QLabel, QFileDialog
 
 
 class Window0(QWidget):
+    """
+    starting window
+    asks for project name
+    """
 
     def __init__(self):
         super().__init__()
@@ -20,6 +28,7 @@ class Window0(QWidget):
         start_btn = QPushButton('Rozpocznij tworzenie talii >>>', self)
         start_btn.setGeometry(275, 275, 250, 50)
         self.show()
+        self.project_name.returnPressed.connect(self.next)
         start_btn.clicked.connect(self.next)
 
     def next(self):
@@ -30,6 +39,9 @@ class Window0(QWidget):
 
 
 class Window1(QWidget):
+    """
+    gets data file
+    """
 
     def __init__(self):
         super().__init__()
@@ -39,23 +51,57 @@ class Window1(QWidget):
         self.resize(800, 600)
         center(self)
         QLabel(
-            'Wybierz plik *.csv z danymi do projektu {}. Dane zostaną skopiowane, plik nie zostanie naruszony.'.format(
+            'Wybierz plik *.csv z danymi do projektu {}. Zostanie tylko wczytany, nie będzie naruszony.'.format(
                 window0.project), self)
         open_btn = QPushButton('Otwórz plik', self)
+        open_btn.setGeometry(325, 250, 150, 100)
         open_btn.clicked.connect(self.open_file)
         self.show()
 
     def open_file(self):
         options = QFileDialog.Options()
         options |= QFileDialog.DontUseNativeDialog
-        filename, _ = QFileDialog.getOpenFileName(self, "Wybierz swój plik z danymi:", "",
-                                                  "dane do cardschooli (*.csv)", options=options)
+        filename = \
+            QFileDialog.getOpenFileName(self, 'Wybierz swój plik z danymi:', filter='dane do cardschooli (*.csv)',
+                                        options=options)[0]
         self.filename = filename
+        print(get_data(filename))
         self.next()
 
     def next(self):
         print(self.filename)
         self.close()
+        window2.init_ui(self.filename)
+
+
+class Window2(QWidget):
+    """
+    user creates his card
+    """
+
+    def __init__(self):
+        super().__init__()
+
+    def init_ui(self, location):
+        self.resize(800, 600)
+        self.setWindowTitle('cardschooli - krok 2')
+        QLabel('Zaprojektuj swoją kartę.', self)
+        center(self)
+        preview = QLabel(self)
+        path = os.path.join(os.pardir, 'cards', window0.project)
+        file = os.path.join(os.pardir, 'cards', window0.project, 'preview.png')
+        print(path, file, location)
+        if not os.path.exists(path):
+            os.makedirs(path)
+        self.make_card_preview(file)
+        pixmap = QPixmap(file)
+        preview.setPixmap(pixmap)
+        self.show()
+
+    def make_card_preview(self, prev_file):
+        prev = Image.new('RGBA', (240, 336), 'white')
+        prev_draw = ImageDraw.Draw(prev)
+        prev.save(prev_file)
 
 
 def center(window):
@@ -64,8 +110,13 @@ def center(window):
     window.move(qr.topLeft())
 
 
+def get_data(location):
+    return list(csv.reader(open(location)))
+
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     window0 = Window0()
     window1 = Window1()
+    window2 = Window2()
     sys.exit(app.exec_())

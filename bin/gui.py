87c51189003bbdf3a9e4b95c7fed4315c2c9ee59
m4k5 <maks@riseup.net>
2018-02-17 20:50:08 +0100

    totally reformatted, written from beginning, new functions, new seperator, divided into smaller parts and much more

---
+++ bin/gui.py
@@ -0,0 +1,361 @@
+# coding=utf-8
+"""
+cardschooli gui
+allows user creating his deck of cards
+"""
+import os
+import sys
+
+from PyQt5.QtGui import QPixmap, QMovie
+from PyQt5.QtWidgets import QApplication, QWidget, QDesktopWidget, QPushButton, QLineEdit, QLabel, QFileDialog, \
+    QColorDialog, QInputDialog, QMessageBox
+
+import fs_interaction
+import obverse
+import reverse
+
+
+def center(window):
+    """ centers window """
+    qr = window.frameGeometry()
+    qr.moveCenter(QDesktopWidget().availableGeometry().center())
+    window.move(qr.topLeft())
+
+
+def file_dialog(parent, caption, *expression, folder=False):
+    """ shows file dialog """
+    options = QFileDialog.Options()
+    options |= QFileDialog.DontUseNativeDialog
+    if folder:
+        filename = QFileDialog.getExistingDirectory(parent=parent, caption=caption)
+        return filename
+    filename = QFileDialog.getOpenFileName(parent=parent, caption=caption, filter=expression[0], options=options)
+    return filename[0] if filename[1] and filename[0].endswith(expression[1]) else None
+
+
+def color_dialog():
+    """ shows color dialog """
+    color = QColorDialog.getColor()
+    if color.isValid():
+        return color.name()
+    return None
+
+
+def size_dialog(parent):
+    """ gets text's size"""
+    i, ok_pressed = QInputDialog.getInt(parent, "Podaj rozmiar czcionki:", "Wielkość czcionki:", 1)
+    if ok_pressed:
+        return i
+
+
+def text_dialog(parent):
+    """ gets text """
+    text, ok_pressed = QInputDialog.getText(parent, "Jaki tekst chcesz dodać?", "Podaj tekst:", QLineEdit.Normal)
+    if ok_pressed and text:
+        return text
+
+
+def raise_warning(parent, caption, text):
+    """ raises PyQt warning """
+    QMessageBox.warning(parent, caption, text, QMessageBox.Ok)
+
+
+def coords_dialog(parent):
+    """ asks user for coords """
+    i, ok_pressed0 = QInputDialog.getInt(parent, "Podaj pozycję w poziomie obiektu:",
+                                         "Anuluj aby wycentrować horyzontalnie. X:", min=0, max=1500)
+    j, ok_pressed1 = QInputDialog.getInt(parent, "Podaj pozycję w pionie obiektu:",
+                                         "Anuluj aby wycentrować wertykalnie. Y:", min=0, max=2100)
+    if not ok_pressed0:
+        i = -1
+    if not ok_pressed1:
+        j = -1
+    return [i, j]
+
+
+def choose_colum(parent, caption, text, selections):
+    response = QInputDialog.getItem(parent, caption, text, selections)
+    if response[1]:
+        return response[0]
+
+
+class Window0(QWidget):
+    """
+    starting window
+    asks user for project name
+    """
+
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("cardschooli - witaj!")
+        self.resize(800, 600)
+        center(self)
+        self.project_name = QLineEdit(self)
+        self.project_name.setToolTip("Wprowadź dowolną nazwę projektu.")
+        self.project_name.move(275, 200)
+        self.project_name.resize(250, 25)
+        self.start_btn = QPushButton("Rozpocznij tworzenie talii >>>", self)
+        self.start_btn.setGeometry(275, 275, 250, 50)
+        self.show()
+        self.project_name.returnPressed.connect(self.next)
+        self.start_btn.clicked.connect(self.next)
+
+    def next(self):
+        if not fs_interaction.project_location(self.project_name.text()):
+            raise_warning(self, "Tylko znaki alfanumeryczne!",
+                          "W nazwie projektu wykorzystałeś znaki niealfanumeryczne. Spróbuj jeszcze raz!")
+            return None
+        self.project = self.project_name.text()
+        self.close()
+        window1.init_ui()
+
+
+class Window1(QWidget):
+    """
+    csv import window
+    asks user for his data file (*.csv)
+    """
+
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("cardschooli - krok 1")
+        self.resize(800, 600)
+        center(self)
+        open_btn = QPushButton("Otwórz plik", self)
+        open_btn.setGeometry(325, 250, 150, 100)
+        open_btn.clicked.connect(self.open_file)
+
+    def init_ui(self):
+        QLabel(
+            "Wybierz plik *.csv z danymi do projektu {}. Zostanie tylko wczytany, nie będzie naruszony. \nKoniecznie "
+            "użyj nagłówków!".format(
+                window0.project), self)
+        self.show()
+
+    def open_file(self):
+        self.filename = file_dialog(self, "Wybierz plik z danymi:", "dane do cardschooli (*.csv)", ".csv")
+        if not self.filename:
+            raise_warning(self, "Nie wybrałeś pliku",
+                          "Nie wybrałeś pliku, bądź ma on nieodpowiedni format. Użyj pliku .csv!")
+            return None
+        self.close()
+        window2.init_ui()
+
+
+class Window2(QWidget):
+    """
+    designing reverse
+    user makes his reverse
+    Features: customizing background color, importing png, adding text
+    """
+
+    def __init__(self):
+        super().__init__()
+        self.resize(800, 600)
+        self.setWindowTitle("cardschooli - krok 2")
+        QLabel(
+            "Zaprojektuj swoją kartę. Zacznijmy od rewersu, czyli tej strony z tyłu. Będzie taki sam dla każdej karty.",
+            self)
+        center(self)
+        color_btn = QPushButton("Wybierz kolor tła", self)
+        image_btn = QPushButton("Zaimportuj grafikę PNG", self)
+        text_btn = QPushButton("Dodaj tekst", self)
+        finish_btn = QPushButton("Dalej >>>", self)
+        color_btn.setGeometry(500, 60, 225, 35)
+        image_btn.setGeometry(500, 115, 225, 35)
+        text_btn.setGeometry(500, 170, 225, 35)
+        finish_btn.setGeometry(500, 475, 225, 70)
+        color_btn.clicked.connect(self.color_btn_act)
+        image_btn.clicked.connect(self.image_btn_act)
+        text_btn.clicked.connect(self.text_btn_act)
+        finish_btn.clicked.connect(self.finish_btn_act)
+
+    def init_ui(self):
+        self.path = fs_interaction.project_location(window0.project, "reverse_preview.png")
+        self.card = reverse.CardReverse(window0.project)
+        self.preview = QLabel(self)
+        self.pixmap = QPixmap(self.path)
+        self.preview.setPixmap(self.pixmap)
+        self.preview.setGeometry(25, (self.height() - self.pixmap.height()) / 2,
+                                 self.pixmap.width(), self.pixmap.height())
+        self.show()
+
+    def update_preview(self):
+        self.pixmap = QPixmap(self.path)
+        self.preview.setPixmap(self.pixmap)
+        self.update()
+
+    def color_btn_act(self):
+        color = color_dialog()
+        if color:
+            self.card.change_color(color)
+            self.update_preview()
+
+    def image_btn_act(self):
+        paste_path = file_dialog(self, "Wybierz obrazek do zaimportowania na rewers:", "PNG (*.png)", ".png")
+        if not paste_path:
+            raise_warning(self, "Nie wybrałeś obrazka!", "Nie wybrałeś obrazka do zaimportowania!")
+            return None
+        coords = coords_dialog(self)
+        self.card.paste(paste_path, coords)
+        self.update_preview()
+
+    def text_btn_act(self):
+        text = text_dialog(self)
+        if not text:
+            return None
+        size = size_dialog(self)
+        if not size:
+            return None
+        coords = coords_dialog(self)
+        if not coords:
+            return None
+        color = color_dialog()
+        if not color:
+            return None
+        if self.card.add_text(coords, text, size, color):
+            raise_warning(self, "Za duży tekst!", "Wybrany przez ciebie tekst nie zmieści się na karcie. Spróbuj "
+                                                  "ponownie z innymi ustawieniami.")
+            return None
+        self.update_preview()
+
+    def finish_btn_act(self):
+        self.close()
+        window3.init_ui()
+
+
+class Window3(QWidget):
+    """
+    Designing obverse, user makes his obverse. Features: customizing background color, importing png, importing png
+    folder, adding text with variables from data file
+    """
+
+    def __init__(self):
+        super().__init__()
+        self.resize(800, 600)
+        self.setWindowTitle("cardschooli - krok 3")
+        QLabel(
+            "Zaprojektuj swoją kartę. Teraz pora zaprojektować awers. Szablon będzie taki sam, "
+            "ale masz\nmożliwość umieszczenia na nim elementów, które dla każdej karty przetworzone zostaną "
+            "na\nodpowiednią wartość z pliku danych.".format(),
+            self)
+        center(self)
+        color_btn = QPushButton("Wybierz kolor tła", self)
+        image_btn = QPushButton("Zaimportuj grafikę PNG", self)
+        image_var_btn = QPushButton("Zaimportuj folder z grafikami PNG", self)
+        text_btn = QPushButton("Dodaj tekst", self)
+        finish_btn = QPushButton("Zakończ >>>", self)
+        color_btn.setGeometry(490, 60, 235, 35)
+        image_btn.setGeometry(490, 115, 235, 35)
+        image_var_btn.setGeometry(490, 170, 235, 35)
+        text_btn.setGeometry(490, 225, 235, 35)
+        finish_btn.setGeometry(490, 475, 235, 70)
+        color_btn.clicked.connect(self.color_btn_act)
+        image_btn.clicked.connect(self.image_btn_act)
+        image_var_btn.clicked.connect(self.image_folder_btn_act)
+        text_btn.clicked.connect(self.text_btn_act)
+        finish_btn.clicked.connect(self.finish_btn_act)
+
+    def init_ui(self):
+        self.path = fs_interaction.project_location(window0.project, "obverse_preview.png")
+        self.card = obverse.CardObverse(window0.project, window1.filename)
+        self.preview = QLabel(self)
+        self.pixmap = QPixmap(self.path)
+        self.preview.setPixmap(self.pixmap)
+        self.preview.setGeometry(25, (self.height() - self.pixmap.height()) / 2,
+                                 self.pixmap.width(), self.pixmap.height())
+        self.show()
+
+    def update_preview(self):
+        self.pixmap = QPixmap(self.path)
+        self.preview.setPixmap(self.pixmap)
+        self.update()
+
+    def color_btn_act(self):
+        color = color_dialog()
+        if color:
+            self.card.change_color(color)
+            self.update_preview()
+
+    def image_btn_act(self):
+        paste_path = file_dialog(self, "Wybierz obrazek do zaimportowania na awers:", "PNG (*.png)", ".png")
+        if not paste_path:
+            raise_warning(self, "Nie wybrałeś obrazka!", "Nie wybrałeś obrazka do zaimportowania!")
+            return None
+        coords = coords_dialog(self)
+        self.card.paste(paste_path, coords)
+        self.update_preview()
+
+    def image_folder_btn_act(self):
+        column = choose_colum(self, "Wybierz kolumnę:",
+                              "Wybierz kolumnę, w której zapisane są nazwy plików PNG dla każdej karty:",
+                              fs_interaction.read_csv_line(window1.filename, 0))
+        if not column:
+            return None
+        column_data = fs_interaction.read_csv_line(window1.filename, 0)
+        column_nr = column_data.index(column)
+        folder = file_dialog(self, "Wybierz folder z obrazkami:", folder=True)
+        if not folder:
+            return None
+        coords = coords_dialog(self)
+        if not coords:
+            return None
+        self.card.add_image_folder(folder, column_nr, coords)
+        self.update_preview()
+
+    def text_btn_act(self):
+        text = text_dialog(self)
+        if not text:
+            return None
+        size = size_dialog(self)
+        if not size:
+            return None
+        coords = coords_dialog(self)
+        if not coords:
+            return None
+        color = color_dialog()
+        if not color:
+            return None
+        if self.card.add_text(coords, text, size, color):
+            raise_warning(self, "Za duży tekst!", "Wybrany przez ciebie tekst nie zmieści się na karcie. Spróbuj "
+                                                  "ponownie z innymi ustawieniami.")
+            return None
+        self.update_preview()
+
+    def finish_btn_act(self):
+        self.close()
+        window4.init_ui()
+
+
+class Window4(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.resize(800, 600)
+        self.setWindowTitle("cardschooli - krok 4")
+        center(self)
+        self.loading = QLabel("Twoja talia jest generowana. Zachowaj cierpliwość, to może chwilę potrwać...", self)
+        self.loading.move(140, 216)
+        self.preloader = QLabel(self)
+        movie = QMovie(os.path.join(os.pardir, "res", "img", "preloader.gif"))
+        self.preloader.setMovie(movie)
+        movie.start()
+        self.preloader.setGeometry(336, 236, 128, 128)
+
+    def init_ui(self):
+        self.compile()
+        self.show()
+
+    def compile(self):
+        obverse.generate(window0.project, window1.filename,
+                         fs_interaction.project_location(window0.project, "obverse.cardconfig"))
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window0 = Window0()
+    window1 = Window1()
+    window2 = Window2()
+    window3 = Window3()
+    window4 = Window4()
+    sys.exit(app.exec_())
---
+++ bin/obverse.py
@@ -0,0 +1,128 @@
+# coding=utf-8
+"""
+generating, updating and saving the card's obverse
+"""
+import os.path
+
+from PIL import Image, ImageDraw, ImageFont
+
+import fs_interaction
+
+
+def process_coords(coords, size, psize):
+    """
+    centers object to be pasted on card
+    :param coords: coords of the object to be pasted
+    :param size: size of the we are pasting on
+    :param psize: size of the object to be pasted
+    :return: proper coords of the object to be pasted
+    """
+    if coords[0] == -1 or coords[1] == -1:
+        if coords[0] == -1:
+            coords[0] = int((size[0] - psize[0]) / 2)
+        if coords[1] == -1:
+            coords[1] = int((size[1] - psize[1]) / 2)
+    return coords
+
+
+def add_command(command, path):
+    """
+    addes command to the command's file
+    :param command: command to be added
+    :param path: path at which command file is located
+    """
+    with open(path, "a") as cf:
+        cf.write(command)
+
+
+def generate(name, data_path, config_path):
+    """ proceeds with creating obverses from config file """
+    obverses = [CardObverse(name, data_path, i) for i in range(fs_interaction.get_file_lenght(data_path) - 1)]
+    cmds = fs_interaction.read_config(config_path)
+    for i in cmds:
+        for j in obverses:
+            if i[0] == "col":
+                j.change_color(i[1], False)
+            elif i[0] == "img":
+                j.paste(i[1], (i[2], i[3]), False)
+            elif i[0] == "imgf":
+                j.add_image_folder(i[1], i[2], (i[3], i[4]), False)
+            elif i[0] == "txt":
+                j.add_text((i[1], i[2]), i[3], i[5], i[6], i[4], False)
+    for i, obv in enumerate(obverses):
+        obv.obverse.save(fs_interaction.project_location(name, fname="obverse{}.png".format(i)))
+
+
+class CardObverse(object):
+    """
+    class used to create obverse of card sized 1500x2100px with a dpi of 600
+    """
+
+    def __init__(self, project_name, data_path, number=1):
+        self.project_name = project_name
+        self.config_path = fs_interaction.project_location(self.project_name, "obverse.cardconfig")
+        self.data_path = data_path
+        self.number = number
+        self.obverse = Image.new("RGB", (1500, 2100), 0)
+        self.obverse_draw = ImageDraw.Draw(self.obverse)
+        self.save_preview()
+
+    def save_preview(self):
+        """ saves scaled preview of obverse (the first one) """
+        tn_obverse = self.obverse.copy()
+        tn_obverse.thumbnail((425, 550))
+        tn_obverse.save(fs_interaction.project_location(self.project_name, "obverse_preview.png"),
+                        format="PNG", dpi=(600, 600))
+
+    def change_color(self, color, gen_cnfg=True):
+        """
+        updates color of the card's obverse
+        :param color: new color specified in RGB
+        :param gen_cnfg: if True will save a command
+        """
+        self.obverse = Image.new("RGB", (1500, 2100), color)
+        self.obverse_draw = ImageDraw.Draw(self.obverse)
+        if gen_cnfg:
+            add_command("col^^{}\n".format(color), self.config_path)
+        self.save_preview()
+
+    def paste(self, image, coords, gen_cnfg=True):
+        """
+        pastes image to card's obverse
+        :param image: image to be pasted
+        :param coords: coords of the pasted image on card
+        :param gen_cnfg: if True will save a command
+        """
+        image = Image.open(image).convert("RGBA")
+        coords = process_coords(coords, self.obverse.size, image.size)
+        self.obverse.paste(image, coords, image)
+        if gen_cnfg:
+            add_command("img^^{}^^{}^^{}\n".format(image, coords[0], coords[1]), self.config_path)
+        self.save_preview()
+
+    def add_image_folder(self, folder_path, column, coords, gen_cnfg=True):
+        row = fs_interaction.read_csv_line(self.data_path, self.number)
+        self.paste(os.path.join(folder_path, row[column] + ".png"), coords, False)
+        if gen_cnfg:
+            add_command("imgf^^{}^^{}^^{}^^{}".format(folder_path, column, coords[0], coords[1]), self.config_path)
+
+    def add_text(self, coords, text, size, fill=0, font=os.path.join(os.pardir, "res", "fonts", "font.ttf"),
+                 gen_cnfg=True):
+        """
+        adds text to card's obverse
+        :param coords: coords of the added text
+        :param text: text to be added
+        :param size: size of the text to be added
+        :param fill: color of the text
+        :param font: path to the font's ttf file
+        :param gen_cnfg: if True will save a command
+        """
+        if gen_cnfg:
+            add_command("txt^^{}^^{}^^{}^^{}^^{}^^{}\n".format(coords[0], coords[1], text, font, size, fill),
+                        self.config_path)
+        font = ImageFont.truetype(font, size)
+        coords = process_coords(coords, self.obverse.size, self.obverse_draw.textsize(text, font))
+        if coords[0] < 0 or coords[1] < 0:
+            return 1
+        self.obverse_draw.text(coords, text, fill, font)
+        self.save_preview()

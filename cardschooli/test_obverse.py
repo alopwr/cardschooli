13dbc72604434212fa22b2351391f34a55834c12
m4k5 <maks@riseup.net>
2018-03-01 20:12:43 +0100

    structure update

--- cardschooli/test_obverse.py
+++ cardschooli/test_obverse.py
@@ -1,9 +1,7 @@
-import sys
+from obverse import process_coords
 
 
 class TestObverse(object):
     def test_process_coords(self):
-        print("\n\n\n\n\n{}\n\n\n\n\n".format(sys.path))
-        from obverse import process_coords
         xy = [0, 0]
         assert process_coords(xy, 0, 0) == [0, 0]

d8de6e476dc6dbbc92b9b6b80840165bf906b1a5
m4k5 <maks@riseup.net>
2018-03-01 19:56:02 +0100

    structure update

--- tests/test_obverse.py
+++ tests/test_obverse.py
@@ -1,7 +1,9 @@
-from obverse import process_coords
+import sys
 
 
 class TestObverse(object):
     def test_process_coords(self):
+        print("\n\n\n\n\n{}\n\n\n\n\n".format(sys.path))
+        from obverse import process_coords
         xy = [0, 0]
         assert process_coords(xy, 0, 0) == [0, 0]

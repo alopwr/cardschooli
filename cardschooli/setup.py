8c539e8bb9049813b6490d199612b4db88f492f7
m4k5 <maks@riseup.net>
2018-03-24 22:01:26 +0100

    added setup.py

---
+++ cardschooli/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='cardschooli',
+    version='erytrocyt',
+    packages=[''],
+    package_dir={'': 'cardschooli'},
+    url='https://github.com/m4k5/cardschooli',
+    license='Apache License Version 2.0',
+    author='m4k5, Simon-the-Shark',
+    author_email='cardschooli@m4k5.me',
+    description='Program which generates educational playing cards for gamification. Our school project.'
+)
